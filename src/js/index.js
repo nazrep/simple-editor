9f7e35d911580ce953cb9fceacd3c0d8c92e62a4
Maciej Perzankowski <perzankowski.m2001@gmail.com>
2020-06-16 14:59:52 +0200

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,16 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const textarea = document.querySelector('.textarea--js');
+const save = document.querySelector('.save--js');
+const load = document.querySelector('.load--js');
+
+save.addEventListener('click', () => {
+    e.preventDefault();
+    localStorage.setItem('entry', textarea.value);
+})
+
+load.addEventListener('click', () => {
+    e.preventDefault();
+    textarea.value = localStorage.getItem('entry');
+})
